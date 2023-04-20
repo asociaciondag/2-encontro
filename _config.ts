44e5b43448f05db7f105dff997dc5029ab376e3c
Oscar Otero <oom@oscarotero.com>
2023-04-20 20:24:49 +0200

    maquetación

--- _config.ts
+++ _config.ts
@@ -1,8 +1,11 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
+import svgo from "lume/plugins/svgo.ts";
 
 const site = lume();
 
 site.use(postcss());
+site.use(svgo());
+site.copy("fonts");
 
 export default site;
