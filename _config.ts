25312486a85c49425696b0af79f340de69ebd603
Oscar Otero <oom@oscarotero.com>
2023-04-25 23:01:42 +0200

    cambios

--- _config.ts
+++ _config.ts
@@ -1,11 +1,15 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import svgo from "lume/plugins/svgo.ts";
+import metas from "lume/plugins/metas.ts";
 
-const site = lume();
+const site = lume({
+  location: new URL("https://dag.gal/2-encontro/"),
+});
 
 site.use(postcss());
 site.use(svgo());
+site.use(metas());
 site.copy("fonts");
 
 export default site;
